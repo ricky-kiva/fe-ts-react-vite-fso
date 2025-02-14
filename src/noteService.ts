1492f2c5b6407072e095ca19aead2c6ead669490
ricky-kiva <ricky.kiva@gmail.com>
2025-02-14 20:22:58 +0700

    feat(src): add service file for `Note`

---
+++ src/noteService.ts
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { Note, NewNote } from './types';
+
+
+const baseUrl = 'http://localhost:3001/api/notes';
+
+export const getAllNotes = () => {
+  return axios
+    .get<Note[]>(baseUrl)
+    .then(res => res.data);
+};
+
+export const createNote = (object: NewNote) => {
+  return axios
+    .post<Note>(baseUrl, object)
+    .then(res => res.data);
+}
